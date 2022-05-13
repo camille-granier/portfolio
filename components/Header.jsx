8d41e466793a3110e110eeeba49d698bcaeba01f
camille-granier <granierc@protonmail.com>
2022-05-14 09:12:57 +1000

    creation of navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import HeaderStyles from './styles/-Header.module.scss';
+import HeaderStyles from '../styles/_Header.module.scss';
 
 const Header = () => {
     return (
         <div className={HeaderStyles.header}>
             <nav>
-
-            </nav>     
+                <a href="#">Home</a>
+                <a href="#about">About me/</a>
+                <a href="#work">Work</a>
+                <a href="#contact">Contact</a>
+            </nav>    
         </div>
     );
 };
