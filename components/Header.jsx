d55fe769004255e5347d05dd1ec95b75559584e8
camille-granier <granierc@protonmail.com>
2022-05-13 17:06:48 +1000

    creation of header

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderStyles from './styles/-Header.module.scss';
+
+const Header = () => {
+    return (
+        <div className={HeaderStyles.header}>
+            <nav>
+
+            </nav>     
+        </div>
+    );
+};
+
+export default Header;
