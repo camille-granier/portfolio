3eb36393b5ce9b838523ea2c8cc3f905468f6676
camille-granier <granierc@protonmail.com>
2022-05-16 15:24:08 +1000

    nav display desktop

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import HeaderStyles from '../styles/_Header.module.scss';
 const Header = () => {
     return (
         <div className={HeaderStyles.header}>
-            <nav>
+            <nav className={HeaderStyles.nav}>
                 <a href="#">Home</a>
                 <a href="#about">About</a>
                 <a href="#work">Work</a>
