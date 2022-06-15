cc23484ba75536e7ad482dcc02a0ad634e7aabb7
camille-granier <granierc@protonmail.com>
2022-06-15 14:22:21 +1000

    adding h1

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import About from './About';
+import React from "react";
+import About from "./About";
 
 const Hero = () => {
-    return (
-        <div className='hero'>
-    <div className='title-container'>
-      <div className='title'>Camille Granier</div>
-     <br/>
-     <div className='sm-title'>Front-end developer</div>
-     </div>
-     <br/>
-    <About />
+  return (
+    <div className="hero">
+      <div className="title-container">
+        <div className="title">Camille Granier</div>
+        <br />
+        <div className="sm-title">Front-end developer</div>
+      </div>
+      <br />
+      <About />
     </div>
-    );
+  );
 };
 
 export default Hero;
--- components/Work.jsx
+++ components/Work.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
-
+import Image from 'next/image';
 const Work = () => {
     return (
-        <div>
-            
+        <div className='work'>
+            <ul>
+                <li>Project 1
+                    <a
+                    href='https://ffnotes.com/'
+                    rel='noreferrer'
+                    target='_blank'>
+                        <Image src='/img/ffnotes.png' 
+                               alt='ffnotes thumbnail'
+                               width={500}
+                               height={263}/>
+                    Final Fantasy Notes
+                    </a>
+                </li>
+                <li>Project 2
+                    <a
+                    href='https://pictures-photo-gallery.herokuapp.com/'
+                    rel='noreferrer'
+                    target='_blank'>
+                        <Image
+                        src='/img/photogallery.png'
+                        alt='photogallery thumbnail'
+                        width={500}
+                        height={263}/>
+                    Photo Gallery
+                    </a>
+                </li>
+                <li>Project 3
+                    <a
+                    href='https://complete-e-shop.web.app/'
+                    rel='noreferrer'
+                    target='_blank'>
+                        <Image 
+                        src='/img/eshop.png'
+                        alt='eshop thumbnail'
+                        width={500}
+                        height={263}/>
+                    E-shop
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
