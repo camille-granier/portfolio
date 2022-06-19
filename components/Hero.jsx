050158725bbbc121e57dc06a1ba7d0fc5c545b66
camille-granier <granierc@protonmail.com>
2022-06-19 15:08:55 +1000

    mobile design of hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,10 +7,8 @@ const Hero = () => {
       <div className="hero-container">
       <div className="title-container">
         <h1 className="title">Camille Granier</h1>
-        <br />
         <div className="sm-title">Front-end developer</div>
       </div>
-      <br />
       <About />
       </div>
     </div>
--- components/Logo.js
+++ components/Logo.js
@@ -3,11 +3,13 @@ import Image from 'next/image';
 
 const Logo = () => (
   <div className='logo'>
+    <a href="#">
     <Image 
     src="/img/cat-2.svg" 
     alt='logo'
     width={500}
     height={500} />
+    </a>
   </div>
 );
 
